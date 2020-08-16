5acfaedf5e78919ecdbd884509df8a9dc6f1cd40
Sazzad Hossain <sharifh530@gmail.com>
2020-08-16 20:17:14 +0600

    Navbar implemented

---
+++ src/pages/about.js
@@ -0,0 +1,19 @@
+import React from "react"
+import Link from "gatsby-link"
+import Layout from "../components/layout"
+
+function AboutPage() {
+  return (
+    <div>
+      <Layout>
+        <h1>About us</h1>
+        <p>
+          afksf ashdffajksfdhs asufhsfhk iashfsdhfsa as asdifgusdfh sdfuif
+          sdgfsdgf safgasif asdfg sjafgasdufgsuf gsdudfgasu df gf asjdf
+        </p>
+      </Layout>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/services.js
@@ -0,0 +1,29 @@
+import React from "react"
+import Layout from "../components/layout"
+
+function services() {
+  return (
+    <div>
+      <Layout>
+        <h1>About us</h1>
+        <p>
+          afksf ashdffajksfdhs asufhsfhk iashfsdhfsa as asdifgusdfh sdfuif
+          sdgfsdgf safgasif asdfg sjafgasdufgsuf gsdudfgasu df gf asjdf afksf
+          ashdffajksfdhs asufhsfhk iashfsdhfsa as asdifgusdfh sdfuif sdgfsdgf
+          safgasif asdfg sjafgasdufgsuf gsdudfgasu df gf asjdfafksf
+          ashdffajksfdhs asufhsfhk iashfsdhfsa as asdifgusdfh sdfuif sdgfsdgf
+          safgasif asdfg sjafgasdufgsuf gsdudfgasu df gf asjdf
+        </p>
+        <p>
+          afksf ashdffajksfdhs asufhsfhk iashfsdhfsa as asdifgusdfh sdfuif
+          sdgfsdgf safgasif asdfg sjafgasdufgsuf gsdudfgasu df gf asjdf safgasif
+          asdfg sjafgasdufgsuf gsdudfgasu df gf asjdf afksf ashdffajksfdhs
+          asufhsfhk iashfsdhfsa as asdifgusdfh sdfuif sdgfsdgf safgasif asdfg
+          sjafgasdufgsuf gsdudfgasu df gf asjdf
+        </p>
+      </Layout>
+    </div>
+  )
+}
+
+export default services
